6a6bc6731933d9811fbfeb8630800812c7952140
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-10-20 03:59:49 +0700

    update detailnews

--- kabar-kampus/src/Pages/DetailNews.jsx
+++ kabar-kampus/src/Pages/DetailNews.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import NavbarPage from "../Components/Navbar";
 
 function DetailNews() {
-  return <div>DetailNews</div>;
+  return (
+    <div className="container">
+      <NavbarPage />
+      <div className="konten">
+        <img
+          src="https://images6.alphacoders.com/121/1214058.png"
+          width={"100%"}
+          alt=""
+        />
+        <label htmlFor="">
+          <h2>Ini Judul Berita</h2>
+        </label>
+        <p>Dan ini isi atau konten</p>
+      </div>
+    </div>
+  );
 }
 
 export default DetailNews;
