7373e00b9faf52ad692e906522a419898ea66b6c
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-10-19 11:56:48 +0700

    add folder pages, add folder components, add react bootstrap

--- kabar-kampus/src/App.jsx
+++ kabar-kampus/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import LoginPage from "./pages/LoginPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <BrowserRouter>
       {
         <Routes>
-          {/* <Route path="/" exact element={<LoginPage />} /> */}
+          <Route path="/" exact element={<LoginPage />} />
         </Routes>
       }
     </BrowserRouter>
