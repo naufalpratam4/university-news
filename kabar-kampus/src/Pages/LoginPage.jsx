19ee5af3c3dec6475331723b45c845bb6d8cc134
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-10-19 12:39:46 +0700

    add a nvabar

--- kabar-kampus/src/Pages/LoginPage.jsx
+++ kabar-kampus/src/Pages/LoginPage.jsx
@@ -1,6 +1,9 @@
+import NavbarPage from "../Components/Navbar";
+
 export default function LoginPage() {
   return (
     <div className="container mt-5">
+      <NavbarPage title="LOGIN" />
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
       voluptas exercitationem nostrum excepturi pariatur quam nisi ex ipsam illo
       distinctio? Animi deleniti velit libero, tempora sapiente labore facilis!
