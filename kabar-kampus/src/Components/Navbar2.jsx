cfdf4328824e851074c0a5fd47d1926d3f70521a
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-10-20 02:15:56 +0700

    add a navbar2

---
+++ kabar-kampus/src/Components/Navbar2.jsx
@@ -0,0 +1,30 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/esm/Button";
+export default function Navbar2() {
+  const titleUniv = "UNIVERSITAS NEGERI SEMARANG";
+
+  return (
+    <>
+      <Navbar data-bs-theme="light" className="pt-4">
+        <Container>
+          <img
+            src="asset/img/Logo_Universitas_Negeri_Semarang.png"
+            alt=""
+            width={"3%"}
+          />
+          <Navbar.Brand href="#home">{titleUniv}</Navbar.Brand>
+          <Nav className="ms-auto fw-bold pe-2">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">News</Nav.Link>
+            <Nav.Link href="#pricing">My News</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+          </Nav>
+          <Button>Login</Button>
+        </Container>
+      </Navbar>
+      <hr style={{ border: "2px solid black" }} />
+    </>
+  );
+}
