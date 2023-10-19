098910176525fc2948372267c535f619285a90f4
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-10-20 01:47:41 +0700

    add component navbar

--- kabar-kampus/src/Components/Navbar.jsx
+++ kabar-kampus/src/Components/Navbar.jsx
@@ -3,24 +3,23 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 export default function NavbarPage(props) {
+  const titleUniv = "UNIVERSITAS NEGERI SEMARANG";
   return (
     <>
       <Navbar data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home" style={{ paddingTop: "20px" }}>
-            {props.title}
+          <img
+            alt=""
+            src="asset/img/Logo_Universitas_Negeri_Semarang.png"
+            width="40"
+          />
+          <Navbar.Brand
+            href="#home"
+            style={{ paddingTop: "20px", paddingLeft: "5px" }}
+          >
+            {titleUniv}
           </Navbar.Brand>
-          <Nav className="ms-auto">
-            <img
-              alt=""
-              src="asset/img/Logo_Universitas_Negeri_Semarang.png"
-              width="40"
-              className="d-inline-block align-top"
-            />{" "}
-            <Navbar.Brand href="#home" style={{ paddingTop: "12px" }}>
-              UNIVERSITAS NEGERI SEMARANG
-            </Navbar.Brand>
-          </Nav>
+          <Nav className="m-auto"></Nav>
         </Container>
       </Navbar>
       <hr style={{ border: "2px solid black" }} />
