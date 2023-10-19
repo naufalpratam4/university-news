d2321aab98b669313c7b645a771aa68c524699db
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-10-19 22:25:59 +0700

    add a HomePage

--- kabar-kampus/src/App.jsx
+++ kabar-kampus/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import LoginPage from "./pages/LoginPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
 const App = () => {
   return (
     <BrowserRouter>
       {
         <Routes>
           <Route path="/" exact element={<LoginPage />} />
+          <Route path="/home" exact element={<HomePage />} />
         </Routes>
       }
     </BrowserRouter>
