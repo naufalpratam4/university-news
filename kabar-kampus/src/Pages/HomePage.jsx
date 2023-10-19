6d5947036d7f7d8d09ab97e9df1267ba7202945f
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-10-20 01:36:11 +0700

    update app

---
+++ kabar-kampus/src/Pages/HomePage.jsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt,
+      maxime. Commodi beatae molestias, quo architecto excepturi eius facilis
+      cum aliquam repellat omnis nobis explicabo libero nisi a! Doloremque,
+      ratione quam.
+    </>
+  );
+}
