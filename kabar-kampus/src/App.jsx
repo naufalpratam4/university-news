bb90633088d5d874616e7d4d30d4d702ab89af76
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-10-20 02:20:52 +0700

    try fix app

--- kabar-kampus/src/App.jsx
+++ kabar-kampus/src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import LoginPage from "./pages/LoginPage";
-import HomePage from "./Pages/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import Navbar2 from "./Components/Navbar2";
 const App = () => {
   return (
     <BrowserRouter>
       {
         <Routes>
           <Route path="/" exact element={<HomePage />} />
+          <Route path="/login" exact element={<LoginPage />} />
+          <Route path="/nav" exact element={<Navbar2 />} />
         </Routes>
       }
     </BrowserRouter>
